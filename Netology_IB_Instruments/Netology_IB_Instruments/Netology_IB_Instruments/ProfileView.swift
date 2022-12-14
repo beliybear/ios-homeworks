ca98f8de18401f4d4d9f8d13d614813610130c46
Belyakov <white.bearr71@gmail.com>
2022-12-14 13:13:39 +0300

    1st homework

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Beliy.Bear on 12.12.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var imageLogo: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var textLabel: UITextView!
+    
+}
